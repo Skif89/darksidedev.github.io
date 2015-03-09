a4d4262281307dbd87e622442f5ff8ff36ca5065
Skif89 <programmer.net.89@gmail.com>
2015-03-09 14:57:49 +0200

    Queen Of Spades

---
+++ QueenOfSpades/scripts/main.js
@@ -0,0 +1,180 @@
+﻿var startSecondsDefault = 10;
+var seconds = startSecondsDefault;
+var selectedCards = [];
+var amount = 3;
+var maxAmount = 54;
+var gameState = 0;
+
+$(document).ready(function () {
+
+    var table = $("#table");
+
+    for (var i = amount; i <= maxAmount; i++)
+        $("#TheNumberOfCards").append($("<option/>", {
+            value: i,
+            text: i
+        }));
+
+    var difficulty = ["Low", "Medium", "Hard", "Insane"];
+
+    for (i = 0; i < difficulty.length; i++)
+        $("#DifficultyLevel").append($("<option/>", {
+            value: difficulty.length - i,
+            text: difficulty[i]
+        }));
+
+    BeforeGame(selectedCards, cards, table, amount);
+
+    $("#ready").bind("click", function () {
+
+        if (gameState === 0) {
+            StartGame(selectedCards, cards, table, amount);
+            $(this).text("I'm ready!");
+            gameState++;
+            startCountdown(seconds, true, false);
+        }
+        else if (gameState === 1) {
+            //Shuffle selected cards
+            var shuffledCards = selectedCards.slice(0);
+            shuffledCards = shuffle(shuffledCards);
+
+            //Clear table
+            table.empty();
+
+            //Draw shuffled cards
+            DrawCards(table, shuffledCards, amount);
+
+            var mistakes = 0;
+
+            var index = 0;
+            $(".card").bind("click", function (event) {
+                if (index !== selectedCards.length) {
+                    if (event.target.id === selectedCards[index]) {
+                        $(this).addClass("cardSelected");
+                        index++;
+                    } else {
+                        if (!$(this).hasClass("cardSelected")) {
+                            mistakes++;
+                            $("#info").empty();
+                            $("#info").append("Mistakes: " + mistakes);
+                        }
+                    }
+                }
+                if (index === selectedCards.length) {
+                    var result = "";
+                    if (mistakes === 0) {
+                        $("#nextStage").show();
+                        result = Win(amount, mistakes);
+                    }
+                    else {
+                        $("#tryAgain").show();
+                        result = Loose(amount, mistakes);
+                    }
+
+                    startTimer(false, true);
+
+                    $("#info").empty();
+                    $("#info").append(result);
+                }
+            });
+
+            $("#mistakes").show();
+            $("#info").empty();
+            $("#info").append("Mistakes: " + mistakes);
+            $("#ready").hide();
+            startTimer(true, true);
+        }
+    });
+
+    $("#TheNumberOfCards").bind("change", (function () {
+        amount = this.value;
+        restartGame();
+    }));
+
+    $("#DifficultyLevel").bind("change", (function () {
+        restartGame();
+    }));
+
+    $("#restart").bind("click", function () {
+        restartGame();
+    });
+
+    $("#tryAgain").bind("click", function () {
+        restartGame();
+    });
+
+    $("#nextStage").bind("click", function () {
+        amount++;
+
+        $("#TheNumberOfCards").val(amount);
+        restartGame();
+    });
+});
+
+var restartGame = function () {
+    gameState = 0;
+    seconds = startSecondsDefault;
+    BeforeGame(selectedCards, cards, $("#table"), amount);
+    $("#ready").text("Start");
+    $("#ready").show();
+    $("#mistakes").hide();
+    $("#nextStage").hide();
+    $("#tryAgain").hide();
+    startTimer(false);
+}
+
+function Info(amount, mistakes) {
+    return "The number of cards: " + amount + "<br />Time: " + getTime() + "<br />Mistakes: " + mistakes;
+}
+
+function Win(amount, mistakes) {
+    return "Congratulations!<br /><br /><img src='images/thumpUp.png'><br /><br />" + Info(amount, mistakes);
+}
+
+function Loose(amount, mistakes) {
+    return "You can do better!<br /><br /><img src='images/thumpDown.png'><br /><br />" + Info(amount, mistakes);
+}
+
+function BeforeGame(selectedCards, cards, table, amount) {
+    selectedCards.length = 0;
+    while (selectedCards.length < amount) {
+        selectedCards.push("backside.png");
+    }
+    //Draw cards
+    DrawCards(table, selectedCards, amount);
+}
+
+function StartGame(selectedCards, cards, table, amount) {
+    selectedCards.length = 0;
+    var difLvl = $("#DifficultyLevel");
+    seconds = difLvl.val() * 5;
+    seconds += ($("#TheNumberOfCards").val() - 2) * difLvl.val() * 2;
+    //Select several cards
+    while (selectedCards.length < amount) {
+        var random = Math.floor((Math.random() * cards.length) + 0);
+
+        if (selectedCards.indexOf(cards[random]) === -1)
+            selectedCards.push(cards[random]);
+    }
+    //Draw shuffled cards
+    DrawCards(table, selectedCards, amount);
+}
+
+function shuffle(o) {
+    var result = o.slice(0);
+    for (var j, x, i = result.length; i; j = Math.floor(Math.random() * i), x = result[--i], result[i] = result[j], result[j] = x);
+
+    if (result.toString() === o.toString())
+        shuffle(o);
+
+    return result;
+};
+
+function DrawCards(table, cards, amount) {
+    table.empty();
+    //Draw cards in Div
+    for (var i = 0; i < amount; i++) {
+        table.append("<img id='" + cards[i] + "' class='card' src='images/cards/" + cards[i] + "'/>");
+    }
+}
+
---
+++ QueenOfSpades/scripts/timer.js
@@ -0,0 +1,80 @@
+﻿var timer;
+var hour;
+var minutes;
+var seconds;
+var countDown;
+
+function startCountdown(seconds, status, direction) {
+    if (status)
+        $("#sample_countdown").show();
+    else {
+        $("#sample_countdown").hide();
+    }
+    var block = document.getElementById('sample_countdown');
+    console.log("startCountdown");
+    simpleTimer(seconds, block, status, direction);
+}
+
+function startTimer(status, direction) {
+    clearTimeout(countDown);
+    if (status)
+        $("#sample_timer").show();
+    else {
+        $("#sample_timer").hide();
+    }
+    $("#sample_countdown").hide();
+    var block = document.getElementById('sample_timer');
+    simpleTimer(0, block, status, direction);
+}
+
+function getTime() {
+    return hour + ':' + minutes + ':' + seconds;
+}
+
+function simpleTimer(sec, block, status, direction) {
+    if (status) {
+        var time = sec;
+        direction = direction || false;
+
+        hour = parseInt(time / 3600);
+        if (hour < 1) hour = 0;
+        time = parseInt(time - hour * 3600);
+        if (hour < 10) hour = '0' + hour;
+
+        minutes = parseInt(time / 60);
+        if (minutes < 1) minutes = 0;
+        time = parseInt(time - minutes * 60);
+        if (minutes < 10) minutes = '0' + minutes;
+
+        seconds = time;
+        if (seconds < 10) seconds = '0' + seconds;
+
+        block.innerHTML = hour + ':' + minutes + ':' + seconds;
+
+        if (direction) {
+            sec++;
+            timer = setTimeout(function () { simpleTimer(sec, block, status, direction); }, 1000);
+        } else {
+            sec--;
+
+            if (sec > 0) {
+                countDown = setTimeout(function () { simpleTimer(sec, block, status, direction); }, 1000);
+            } else {
+                TimeIsUp();
+            }
+        }
+    } else {
+        clearTimeout(timer);
+        clearTimeout(countDown);
+        block.innerHTML = "00:00:00";
+    }
+}
+
+function TimeIsUp() {
+    $("#info").empty();
+    $("#mistakes").show();
+    $("#ready").hide();
+    $("#sample_countdown").hide();
+    $("#tryAgain").show();
+    $("#info").append("Oops, time is up! Please try again. <br /><img width='128px' style='margin-top:10px;' src='images/sadness.png'><br />");
+}
