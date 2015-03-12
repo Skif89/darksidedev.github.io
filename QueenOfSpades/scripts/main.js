2e28850f2639812cb3ba7a96d4caf71a2b815746
Skif89 <programmer.net.89@gmail.com>
2015-03-12 17:28:52 +0200

    A new type of game - Extra card

--- QueenOfSpades/scripts/main.js
+++ QueenOfSpades/scripts/main.js
@@ -6,47 +6,41 @@ var maxAmount = 54;
 var gameState = 0;
 
 $(document).ready(function () {
-    
     var table = $("#table");
-
     Initialization();
-    
+
+    //Set default size of cards (medium)
     $("#CardSize").val("3");
 
     BeforeGame(selectedCards, cards, table, amount);
 
     $("#ready").bind("click", function () {
-
+        var gameType = $("#GameType").val();
         if (gameState === 0) {
             StartGame(selectedCards, cards, table, amount);
             $(this).text("I'm ready!");
             gameState++;
             startCountdown(seconds, true, false);
-        }
-        else if (gameState === 1) {
-            //Clear table
-            table.empty();
-
-            //Shuffle selected cards
-            var shuffledCards = selectedCards.slice(0);
-            shuffledCards = shuffle(shuffledCards);
-
-            //Draw shuffled cards
-            DrawCards(table, shuffledCards, amount);
-
-            var mistakes = 0;
-
-            CardClick(mistakes);
-
-            $("#mistakes").show();
-            $("#info").empty();
-            $("#info").append("Mistakes: " + mistakes);
-            $("#ready").hide();
-            startTimer(true, true);
+        } else if (gameState === 1) {
+            switch (gameType) {
+                case "1":
+                    Game_CorrectSequence(table);
+                    break
+                case "2":
+                    Game_ExtraCards(table);
+                    break
+            }
         }
     });
 
-    $("#TheNumberOfCards").bind("change", (function () {
+    var numberOfCards = $("#TheNumberOfCards");
+
+    $("#GameType").bind("change", (function () {
+        amount = numberOfCards.val();
+        restartGame();
+    }));
+
+    numberOfCards.bind("change", (function () {
         amount = this.value;
         restartGame();
     }));
@@ -70,10 +64,69 @@ $(document).ready(function () {
     $("#nextStage").bind("click", function () {
         amount++;
 
-        $("#TheNumberOfCards").val(amount);
+        numberOfCards.val(amount);
         restartGame();
     });
 });
+
+function restartGame() {
+    BeforeGame(selectedCards, cards, $("#table"), amount);
+    gameState = 0;
+    seconds = startSecondsDefault;
+    $("#ready").text("start");
+    $("#ready").show();
+    $("#mistakes").hide();
+    $("#nextStage").hide();
+    $("#tryAgain").hide();
+    startTimer(false);
+}
+
+function BeforeGame(selectedCards, cards, table, amount) {
+    selectedCards.length = 0;
+    while (selectedCards.length < amount) {
+        selectedCards.push("backside.png");
+    }
+    //Draw cards
+    DrawCards(table, selectedCards);
+}
+
+function Game_CorrectSequence(table) {
+    //Clear table
+    table.empty();
+    //Shuffle selected cards
+    var shuffledCards = selectedCards.slice(0);
+    shuffledCards = shuffle(shuffledCards);
+    //Draw shuffled cards
+    DrawCards(table, shuffledCards);
+    var mistakes = 0;
+    CardClick(mistakes);
+    $("#mistakes").show();
+    $("#info").empty();
+    $("#info").append("Mistakes: " + mistakes);
+    $("#ready").hide();
+    startTimer(true, true);
+}
+
+function Game_ExtraCards(table) {
+    //Clear table
+    table.empty();
+    var shuffledCards = selectedCards.slice(0);
+    while (shuffledCards.length === selectedCards.length) {
+        var card = GetRandomCard();
+        AddCardIfNotExists(shuffledCards, card);
+    }
+    shuffledCards = shuffle(shuffledCards);
+    //Draw shuffled cards
+    DrawCards(table, shuffledCards);
+    var mistakes = 0;
+    ExtraCardClick(mistakes);
+
+    $("#mistakes").show();
+    $("#info").empty();
+    $("#info").append("Mistakes: " + mistakes);
+    $("#ready").hide();
+    startTimer(true, true);
+}
 
 function GetCardSize() {
     return $("#CardSize").val() * 50 + 20 + 'px';
@@ -82,6 +135,25 @@ function GetCardSize() {
 function SetCardsSize() {
     var size = GetCardSize();
     $(".card").css('width', size);
+}
+
+function ExtraCardClick(mistakes) {
+    var index = 0;
+    $(".card").bind("click", function (event) {
+        var result = "";
+        if (selectedCards.indexOf(event.target.id) === -1) {
+            $(this).addClass("cardSelected");
+            result = Win(amount, 0);
+        } else {
+            result = Loose(amount, 0);
+        }
+        $("#tryAgain").show();
+        $("#nextStage").show();
+        $("#info").empty();
+        $("#info").append(result);
+
+        startTimer(false, true);
+    });
 }
 
 function CardClick(mistakes) {
@@ -117,8 +189,6 @@ function CardClick(mistakes) {
 }
 
 function Initialization() {
-   
-
     for (var i = amount; i <= maxAmount; i++)
         $("#TheNumberOfCards").append($("<option/>", {
             value: i,
@@ -135,22 +205,9 @@ function Initialization() {
     var size = ["Tiny", "Small", "Medium", "Large", "Huge"];
     for (i = 0; i < size.length; i++)
         $("#CardSize").append($("<option/>", {
-            value: i+1,
+            value: i + 1,
             text: size[i]
         }));
-}
-
-function restartGame() {
-    BeforeGame(selectedCards, cards, $("#table"), amount);
-    gameState = 0;
-    seconds = startSecondsDefault;
-    $("#ready").text("Start");
-    $("#ready").show();
-    $("#mistakes").hide();
-    $("#nextStage").hide();
-    $("#tryAgain").hide();
-    startTimer(false);
-    
 }
 
 function Info(amount, mistakes) {
@@ -165,36 +222,37 @@ function Loose(amount, mistakes) {
     return "You can do better!<br /><img src='images/thumpDown.png' style='margin:1.1em;'><br />" + Info(amount, mistakes);
 }
 
-function BeforeGame(selectedCards, cards, table, amount) {
-    selectedCards.length = 0;
-    while (selectedCards.length < amount) {
-        selectedCards.push("backside.png");
-    }
-    //Draw cards
-    DrawCards(table, selectedCards, amount);
-}
-
 function StartGame(selectedCards, cards, table, amount) {
     selectedCards.length = 0;
     var difLvl = $("#DifficultyLevel");
     seconds = difLvl.val() * 5;
     seconds += ($("#TheNumberOfCards").val() - 2) * difLvl.val() * 2;
+
     //Select several cards
     while (selectedCards.length < amount) {
-        var random = Math.floor((Math.random() * cards.length) + 0);
-        var c = cards[random];
-        if (selectedCards.indexOf(c) === -1)
-            selectedCards.push(c);
+        var c = GetRandomCard();
+        AddCardIfNotExists(selectedCards, c);
     }
+
     //Draw shuffled cards
-    DrawCards(table, selectedCards, amount);
+    DrawCards(table, selectedCards);
+}
+
+function GetRandomCard() {
+    var random = Math.floor((Math.random() * cards.length) + 0);
+    return cards[random];
+}
+
+function AddCardIfNotExists(cards, card) {
+    if (cards.indexOf(card) === -1)
+        cards.push(card);
 }
 
 function shuffle(o) {
     var result = o.slice(0);
 
     for (var i = result.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * i); // no +1 here!
+        var j = Math.floor(Math.random() * i);
         var temp = result[i];
         result[i] = result[j];
         result[j] = temp;
@@ -203,14 +261,13 @@ function shuffle(o) {
     return result;
 };
 
-function DrawCards(table, cards, amount) {
+function DrawCards(table, cards) {
     table.empty();
     //Draw cards in Div
     var size = GetCardSize();
     //SetCardsSize();
-    for (var i = 0; i < amount; i++) {
+    for (var i = 0; i < cards.length; i++) {
         table.append("<img id='" + cards[i] + "' class='card' style='width:" + size + ";' src='images/cards/" + cards[i] + "'/>");
     }
-   
 }
 
